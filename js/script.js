6cd0f22d982aa2dd2f8e30a68ee5e0e3bfa5aeaf
Angelo Ragusa <angeloragu@yahoo.com>
2017-10-05 12:39:01 -0400

    js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-﻿/* Smooth Scroll */
+/* Smooth Scroll */
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -15,3 +15,12 @@ $(function() {
 });
 
 /* End Smooth Scroll */
+
+/* Fixed Nav on Scroll */
+$(window).scroll( function( e ){ 
+    if( $(this).scrollTop() > $('.header-container').offset().top ){
+        $(".nav-container").addClass("nav-fixed");
+    } else {
+        $(".nav-container").removeClass("nav-fixed");
+    }
+});
